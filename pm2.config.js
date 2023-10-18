4e0e15df30d817aa211cdeb8a3f681ec8ffce11e
RayAuxey <rayauxey@gmail.com>
2023-10-18 14:25:49 +0300

    Change name

--- pm2.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  name: "spcl-image-uploader", // Name of your application
-  script: "src/index.ts", // Entry point of your application
-  interpreter: "~/.bun/bin/bun", // Path to the Bun interpreter
-};
